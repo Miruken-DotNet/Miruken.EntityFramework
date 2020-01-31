c6c30539a5467c837b045ef7906def8a5f0914b2
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2020-01-31 12:20:53 -0600

    Added dbcontext and parameter helpers for stored procs

---
+++ Source/Miruken.EntityFramework/IDbContextExtensions.cs
@@ -0,0 +1,49 @@
+﻿namespace Miruken.EntityFramework
+{
+    using System.Data;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.Data.SqlClient;
+    using Microsoft.EntityFrameworkCore;
+
+    public static class IDBContextExtensions
+    {
+        public static IQueryable<T> ExecuteStoredProcedure<T>(
+            this IDbContext       context,
+            string                procedureName,
+            params SqlParameter[] parameters) 
+            where T : class
+        {
+            return context.Set<T>()
+                .FromSqlRaw($"EXECUTE {procedureName} {StringifySqlParameters(parameters)}", parameters);
+        }
+
+        public static int ExecuteStoredProcedure(
+            this IDbContext       context, 
+            string                procedureName,
+            params SqlParameter[] parameters)
+        {
+            return context.Database
+                .ExecuteSqlRaw($"EXECUTE {procedureName} {StringifySqlParameters(parameters)}", parameters);
+        }
+
+        private static string StringifySqlParameters(SqlParameter[] dbParams)
+        {
+            if (dbParams == null || dbParams.Length == 0)
+                return string.Empty;
+            var sb = new StringBuilder();
+            for (var i = 0; i < dbParams.Length; i++)
+            {
+                var param = dbParams[i];
+                sb.Append(i == 0 ? " @" : ", @");
+                sb.Append($"{param.ParameterName} = @{param.ParameterName}");
+                if (param.Direction == ParameterDirection.InputOutput ||
+                    param.Direction == ParameterDirection.Output)
+                {
+                    sb.Append(" OUTPUT");
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Source/Miruken.EntityFramework/SqlParameterExtensions.cs
@@ -0,0 +1,68 @@
+﻿namespace Miruken.EntityFramework
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using Microsoft.Data.SqlClient;
+
+    public static class SqlParameterExtensions
+    {
+        public static ICollection<SqlParameter> AddParameter(
+            this ICollection<SqlParameter> paramList,
+            SqlParameter                   param)
+        {
+            if (paramList == null)
+                throw new ArgumentNullException(nameof(paramList));
+
+            if (paramList.IsReadOnly)
+            {
+                throw new InvalidOperationException(
+                    "Cannot add parameters to a Read - Only Collection.");
+            }
+
+            if (param != null) paramList.Add(param);
+            return paramList;
+        }
+
+        public static ICollection<SqlParameter> AddParameter(
+            this ICollection<SqlParameter> paramList,
+            string                         paramName,
+            SqlDbType                      type,
+            object value)
+        {
+            if (paramList == null)
+                throw new ArgumentNullException(nameof(paramList));
+
+            if (paramList.IsReadOnly)
+            {
+                throw new InvalidOperationException(
+                    "Cannot add parameters to a Read - Only Collection.");
+            }
+
+            var param = new SqlParameter { ParameterName = paramName, SqlDbType = type, Value = value ?? DBNull.Value };
+            return paramList.AddParameter(param);
+        }
+
+        public static ICollection<SqlParameter> AddParameter<T>(
+            this ICollection<SqlParameter> paramList, string paramName, SqlDbType type, T? value) where T : struct
+        {
+            if (paramList == null)
+                throw new ArgumentNullException(nameof(paramList));
+
+            if (paramList.IsReadOnly)
+            {
+                throw new InvalidOperationException(
+                    "Cannot add parameters to a Read - Only Collection.");
+            }
+
+            var param = new SqlParameter(paramName, type);
+
+            if (value.HasValue)
+                param.Value = value.Value;
+            else
+                param.Value = DBNull.Value;
+
+            return paramList.AddParameter(param);
+        }
+    }
+}
